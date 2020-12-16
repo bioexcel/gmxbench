89a63fe7da88b42d8f82a4be79aa4f8c63960008
Arno Proeme <a.proeme@epcc.ed.ac.uk>
2020-12-16 13:40:20 +0000

    Separate out GPU offload plots

--- gmxbench.sh
+++ gmxbench.sh
@@ -9,8 +9,6 @@
 
 
 
-
-
 usage()
 {
     echo -e "\t usage: 
@@ -105,7 +103,7 @@ function set_options()
 		HELP=true
 		shift 2 ;;
 	    --bench)
-		BENCHMARK=`readlink -f $2`
+		BENCHMARK=$(readlink -f $2)
 		shift 2 ;;
 	    --exe)
 		EXE="$2"
@@ -205,7 +203,7 @@ function init_params()
     # Ensure npme set to 1 if offloading PME to GPU
     if [ "$PME" = 'gpu' ] && [ "$TOTALRANKS" != "1" ]; then
 	NPME_CMD='-npme 1 '
-	NPME_STR='_npme001'
+#	NPME_STR='_npme001'
     elif test -n "$NPME"; then
 	NPME_CMD="-npme $NPME "
 	NPME_STR="_npme$(printf "%03d" $NPME)"
@@ -229,21 +227,33 @@ function init_params()
     
     
     # GPU offload options
-    if [ "$GPU" == true ]; then
-	NB_CMD="-nb $NB "
-	NB_STR="_nb`echo $NB | tr [a-z] [A-Z]`"
-	PME_CMD="-pme $PME "
-	PME_STR="_pme`echo $PME | tr [a-z] [A-Z]`"
-	BONDED_CMD="-bonded $BONDED "
-	BONDED_STR="_bonded`echo $BONDED | tr [a-z] [A-Z]`"
-	UPDATE_CMD="-update $UPDATE "
-	UPDATE_STR="_update`echo $UPDATE | tr [a-z] [A-Z]`"
-	PMEFFT_CMD="-pmefft $PMEFFT "
-	PMEFFT_STR="_pmefft`echo $PMEFFT | tr [a-z] [A-Z]`"
-	GPU_OFFLOAD_CMD=${NB_CMD}${PME_CMD}${NPME_CMD}${BONDED_CMD}${UPDATE_CMD}
-	GPU_OFFLOAD_STR=${NB_STR}${PME_STR}${NPME_STR}${BONDED_STR}${UPDATE_STR}
+    if [ "${GPU}" == true ]; then
+	if test -n "${NB}"; then
+	    NB_CMD="-nb ${NB} "
+	    NB_STR="_nb$(echo ${NB} | tr [a-z] [A-Z])"
+	fi
+	if test -n "${PME}"; then
+	    PME_CMD="-pme ${PME} "
+	    PME_STR="_pme$(echo ${PME} | tr [a-z] [A-Z])"
+	fi
+	if test -n "${BONDED}"; then
+	    BONDED_CMD="-bonded ${BONDED} "
+	    BONDED_STR="_bonded$(echo ${BONDED} | tr [a-z] [A-Z])"
+	fi
+	if test -n "${UPDATE}"; then
+	    UPDATE_CMD="-update ${UPDATE} "
+	    UPDATE_STR="_update$(echo ${UPDATE} | tr [a-z] [A-Z])"
+	fi
+	if test -n "${PMEFFT}"; then
+	    PMEFFT_CMD="-pmefft ${PMEFFT} "
+	    PMEFFT_STR="_pmefft$(echo ${PMEFFT} | tr [a-z] [A-Z])"
+	fi
+	
+	GPU_OFFLOAD_CMD=${NB_CMD}${PME_CMD}${BONDED_CMD}${UPDATE_CMD}${NPME_CMD}
+	GPU_OFFLOAD_STR=${NB_STR}${PME_STR}${BONDED_STR}${UPDATE_STR}
     fi
-
+    
+    
     if test -n "$NSTEPS"; then
 	NSTEPS_CMD="-nsteps $NSTEPS "
 	NSTEPS_STR="_nsteps$(printf "%06d" $NSTEPS)"
@@ -262,18 +272,18 @@ function init_params()
     fi
     if test -n "$DLB"; then
 	DLB_CMD="-dlb $DLB "
-	DLB_STR="_dlb`echo $DLB | tr [a-z] [A-Z]`"
+	DLB_STR="_dlb$(echo $DLB | tr [a-z] [A-Z])"
     fi
     if test -n "$TUNEPME"; then
 	TUNEPME_CMD="-tunepme $TUNEPME "
-	TUNEPME_STR="_tunepme`echo $TUNEPME | tr [a-z] [A-Z]`"
+	TUNEPME_STR="_tunepme$(echo $TUNEPME | tr [a-z] [A-Z])"
     fi
     
     ALWAYS="-noconfout -v"
     PARALLEL_EXECUTION=${SMT_STR}${RANKSPERNODE_STR}${THREADSPERRANK_STR}${NODES_STR}
     MDRUN_CMD=${THREADSPERRANK_CMD}${GPU_OFFLOAD_CMD}${NTOMP_PME_CMD}${NSTEPS_CMD}${RESETSTEP_CMD}${RESETHWAY_CMD}${DLB_CMD}${TUNEPME_CMD}${ALWAYS}
     MDRUN_STR=${GPU_OFFLOAD_STR}${NTOMP_PME_STR}${NSTEPS_STR}${RESETSTEP_STR}${RESETHWAY_STR}${DLB_STR}${TUNEPME_STR}
-    BENCHMARK_NAME=`basename ${BENCHMARK} .tpr`
+    BENCHMARK_NAME=$(basename ${BENCHMARK} .tpr)
     RUN_DESCRIPTOR=${BENCHMARK_NAME}${PARALLEL_EXECUTION}${MDRUN_STR}${EXTRA_INFO}
 }
 
@@ -293,33 +303,33 @@ function descriptor_to_options()
 {
     local descriptor=$1
     descriptor_remainder=${descriptor#"$BENCHMARK_NAME"}
-    local smt_position=`echo $descriptor_remainder | grep -ob "smt" | cut -d ":" -f 1`
-    SMT=`echo ${descriptor_remainder:$((smt_position-1)):1} | cut -d '_' -f 1`
-    local rankspernode_position=`echo $descriptor_remainder | grep -ob "mpi" | cut -d ":" -f 1`
-    RANKSPERNODE=`echo ${descriptor_remainder:$((rankspernode_position-3)):3} | cut -d '_' -f 1`
+    local smt_position=$(echo $descriptor_remainder | grep -ob "smt" | cut -d ":" -f 1)
+    SMT=$(echo ${descriptor_remainder:$((smt_position-1)):1} | cut -d '_' -f 1)
+    local rankspernode_position=$(echo $descriptor_remainder | grep -ob "mpi" | cut -d ":" -f 1)
+    RANKSPERNODE=$(echo ${descriptor_remainder:$((rankspernode_position-3)):3} | cut -d '_' -f 1)
     RANKSPERNODE=$((10#$RANKSPERNODE)) # strip leading zeros
-    local threadsperrank_position=`echo $descriptor_remainder | grep -ob "omp" | cut -d ":" -f 1`
-    THREADSPERRANK=`echo ${descriptor_remainder:$((threadsperrank_position-3)):3} | cut -d '_' -f 1`
+    local threadsperrank_position=$(echo $descriptor_remainder | grep -ob "omp" | cut -d ":" -f 1)
+    THREADSPERRANK=$(echo ${descriptor_remainder:$((threadsperrank_position-3)):3} | cut -d '_' -f 1)
     THREADSPERRANK=$((10#$THREADSPERRANK)) # strip leading zeros
-    local nodes_position=`echo $descriptor_remainder | grep -ob "nodes" | cut -d ":" -f 1`
+    local nodes_position=$(echo $descriptor_remainder | grep -ob "nodes" | cut -d ":" -f 1)
     if test -n "$nodes_position"; then
-	NODES=`echo ${descriptor_remainder:$((nodes_position-2)):2} | cut -d '_' -f 1`
+	NODES=$(echo ${descriptor_remainder:$((nodes_position-2)):2} | cut -d '_' -f 1)
 	NODES=$((10#$NODES))
     fi
     
     if [ "$GPU" == true ]; then
-	local nb_position=`echo $descriptor_remainder | grep -ob "nb" | cut -d ":" -f 1`
-	local nb=`echo ${descriptor_remainder:$nb_position} | cut -d '_' -f 1`
-	NB=`echo ${nb:2} | tr [A-Z] [a-z]` # CPU or GPU
-	local pme_position=`echo $descriptor_remainder | grep -ob "_pme" | cut -d ":" -f 1`
-	local pme=`echo ${descriptor_remainder:$((pme_position+1))} | cut -d '_' -f 1`
-	PME=`echo ${pme:3} | tr [A-Z] [a-z]` # CPU or GPU
-	local bonded_position=`echo $descriptor_remainder | grep -ob "bonded" | cut -d ":" -f 1`
-	local bonded=`echo ${descriptor_remainder:$bonded_position} | cut -d '_' -f 1`
-	BONDED=`echo ${bonded:6} | tr [A-Z] [a-z]` # CPU or GPU
-	local update_position=`echo $descriptor_remainder | grep -ob "update" | cut -d ":" -f 1`
-	local update=`echo ${descriptor_remainder:$update_position} | cut -d '_' -f 1`
-	UPDATE=`echo ${update:6} | tr [A-Z] [a-z]` # CPU or GPU
+	local nb_position=$(echo $descriptor_remainder | grep -ob "nb" | cut -d ":" -f 1)
+	local nb=$(echo ${descriptor_remainder:$nb_position} | cut -d '_' -f 1)
+	NB=$(echo ${nb:2} | tr [A-Z] [a-z]) # CPU or GPU
+	local pme_position=$(echo $descriptor_remainder | grep -ob "_pme" | cut -d ":" -f 1)
+	local pme=$(echo ${descriptor_remainder:$((pme_position+1))} | cut -d '_' -f 1)
+	PME=$(echo ${pme:3} | tr [A-Z] [a-z]) # CPU or GPU
+	local bonded_position=$(echo $descriptor_remainder | grep -ob "bonded" | cut -d ":" -f 1)
+	local bonded=$(echo ${descriptor_remainder:$bonded_position} | cut -d '_' -f 1)
+	BONDED=$(echo ${bonded:6} | tr [A-Z] [a-z]) # CPU or GPU
+	local update_position=$(echo $descriptor_remainder | grep -ob "update" | cut -d ":" -f 1)
+	local update=$(echo ${descriptor_remainder:$update_position} | cut -d '_' -f 1)
+	UPDATE=$(echo ${update:6} | tr [A-Z] [a-z]) # CPU or GPU
     fi
 }
 
@@ -339,7 +349,7 @@ function submit()
 	usage_submit
     fi
     
-    DAYTIME=`date +%j%H%M`
+    DAYTIME=$(date +%j%H%M)
     WORKDIR=${RUN_DESCRIPTOR}-${DAYTIME}
     
     echo -e "\nSubmitting job: "
@@ -429,7 +439,7 @@ function scan()
 		# and adjust to user fewer ranks per node if that is the case
 		# TODO: keep checking (don't step at reducing by 1)
 		#local PPRANKSPERNODE=$(($RANKSPERNODE-1))
-		#local largestdivisor=`factor $PPRANKSPERNODE | rev | cut -d ' ' -f1`
+		#local largestdivisor=$(factor $PPRANKSPERNODE | rev | cut -d ' ' -f1)
 		#if [ "$((largestdivisor**3))" -gt "$((PPRANKSPERNODE**2))" ]; then
 		#    RANKSPERNODE=$RANKSPERNODE-1
 		#fi
@@ -509,31 +519,31 @@ function extract_logfile()
     rm -f ${descriptor}/*.dat
     local logfile=${descriptor}/md.log
 
-    local line=`grep "Running on " $logfile`
-    local nodes=`echo $line | tr -s ' ' | cut -d ' ' -f 3`
+    local line=$(grep "Running on " $logfile)
+    local nodes=$(echo $line | tr -s ' ' | cut -d ' ' -f 3)
     
-    line=`grep "Time:  " $logfile`
-    local wtime=`echo $line | tr -s ' ' | cut -d ' ' -f 3`
+    line=$(grep "Time:  " $logfile)
+    local wtime=$(echo $line | tr -s ' ' | cut -d ' ' -f 3)
     
-    line=`grep "Performance:  " $logfile`
-    local nsperday=`echo $line | tr -s ' ' | cut -d ' ' -f 2`
-    local hoursperns=`echo $line | tr -s ' ' | cut -d ' ' -f 3`
+    line=$(grep "Performance:  " $logfile)
+    local nsperday=$(echo $line | tr -s ' ' | cut -d ' ' -f 2)
+    local hoursperns=$(echo $line | tr -s ' ' | cut -d ' ' -f 3)
     
-    line=`grep "Part of the total run time spent waiting due to load imbalance:" $logfile`
-    local dd_load_imbalance=`echo $line | tr -s ' ' | cut -d ' ' -f 13 | cut -d '%' -f 1`
+    line=$(grep "Part of the total run time spent waiting due to load imbalance:" $logfile)
+    local dd_load_imbalance=$(echo $line | tr -s ' ' | cut -d ' ' -f 13 | cut -d '%' -f 1)
     #echo -e "$nodes \t $dd_load_imbalance" >>  ${descriptor}/ddli.dat
     
-    line=`grep "Part of the total run time spent waiting due to PP/PME imbalance:" $logfile`
-    local pme_load_imbalance=`echo $line | tr -s ' ' | cut -d ' ' -f 13 | cut -d '%' -f 1`
+    line=$(grep "Part of the total run time spent waiting due to PP/PME imbalance:" $logfile)
+    local pme_load_imbalance=$(echo $line | tr -s ' ' | cut -d ' ' -f 13 | cut -d '%' -f 1)
     #echo -e "$nodes \t $pme_load_imbalance" >> ${descriptor}/pmeli.dat
     
     local total_load_imbalance=0
     
     if test -n "$dd_load_imbalance"; then
-	total_load_imbalance=`echo "scale=2; $total_load_imbalance + $dd_load_imbalance" | bc -l`
+	total_load_imbalance=$(echo "scale=2; $total_load_imbalance + $dd_load_imbalance" | bc -l)
     fi
     if test -n "$pme_load_imbalance"; then
-	total_load_imbalance=`echo "scale=2; $total_load_imbalance + $pme_load_imbalance" | bc -l`
+	total_load_imbalance=$(echo "scale=2; $total_load_imbalance + $pme_load_imbalance" | bc -l)
     fi
     
     #local wtime_min=0
@@ -541,12 +551,12 @@ function extract_logfile()
     #local hoursperns_min=0
     
     if [ "$total_load_imbalance" != 0 ]; then
-	#local wtime_imbalance=`echo "scale=2; $wtime*($total_load_imbalance/100.0)" | bc -l`
-	#wtime_min=`echo "scale=2; $wtime - $wtime_imbalance" | bc -l`
-	local nsperday_imbalance=`echo "scale=2; $nsperday*($total_load_imbalance/100.0)" | bc -l`
-	nsperday_max=`echo "scale=2; $nsperday + $nsperday_imbalance" | bc -l`
-	#local hoursperns_imbalance=`echo "scale=2; $hoursperns*($total_load_imbalance/100.0)" | bc -l`
-	#hoursperns_min=`echo "scale=2; $hoursperns - $hoursperns_imbalance" | bc -l`
+	#local wtime_imbalance=$(echo "scale=2; $wtime*($total_load_imbalance/100.0)" | bc -l)
+	#wtime_min=$(echo "scale=2; $wtime - $wtime_imbalance" | bc -l)
+	local nsperday_imbalance=$(echo "scale=2; $nsperday*($total_load_imbalance/100.0)" | bc -l)
+	nsperday_max=$(echo "scale=2; $nsperday + $nsperday_imbalance" | bc -l)
+	#local hoursperns_imbalance=$(echo "scale=2; $hoursperns*($total_load_imbalance/100.0)" | bc -l)
+	#hoursperns_min=$(echo "scale=2; $hoursperns - $hoursperns_imbalance" | bc -l)
     else
 	#wtime_min=$wtime
 	nsperday_max=$nsperday
@@ -575,7 +585,7 @@ function extract_logfile()
 function average_over_runs()
 {
     local run_descriptor=${1}
-    local successful_identical_runs=$(grep "Finished" ${run_descriptor}*/md.log | wc -l)
+    local successful_identical_runs=$(grep --no-messages "Finished" ${run_descriptor}*/md.log | wc -l)
 
     if [ "${successful_identical_runs}" -gt "0" ]; then
 	local extracted_datafiles=$(ls ${run_descriptor}*/nsperday.dat | wc -l)
@@ -613,7 +623,7 @@ function average_over_runs()
 
 
 
-# Extract results from log files in subdirectories of working directory
+# Extract results from subdirectories of working directory
 # for benchmark specified with --bench 
 #
 # For each combination of RANKSPERNODE and THREADSPERRANK,
@@ -634,10 +644,11 @@ function average_over_runs()
 function extract()
 {
     shopt -s extglob
-
+    
     rm -f ${BENCHMARK_NAME}${SMT_STR}*${MDRUN_STR}${EXTRA_INFO}*.dat
     
-    for run in ${BENCHMARK_NAME}${SMT_STR}*${MDRUN_STR}${EXTRA_INFO}*
+    # gather results for all node counts into single .dat file 
+    for run in ${BENCHMARK_NAME}${SMT_STR}*"nodes"*${MDRUN_STR}${EXTRA_INFO}* 
     do
 	run_descriptor=${run%%"-"+([0-9])}  # strip unique DAYTIME identifier
 	
@@ -660,8 +671,6 @@ function extract()
 	    already_extracted=${run_descriptor}
 	fi
     done
-    
-
 }
 
 
@@ -675,31 +684,34 @@ function extract()
 
 function gnuplot_init()
 {
-    GNUPLOT_XLABEL="set xlabel 'nodes'; "
-    GNUPLOT_TERMINAL="set terminal svg size 640,480 name '$GNUPLOT_BASENAME'; "
-    if [ "$GPU" == true ]; then
-	GNUPLOT_KEY="set key left top; "
-    else
-	GNUPLOT_KEY="set key left top maxrows 4; "
-    fi
-    GNUPLOT_ERRORBARS="set errorbars 2.0; "
-    GNUPLOT_OFFSET="set auto fix; set offsets graph 0.05, 0.05, 0, 0; "
-    GNUPLOT_XTICS="set xtics 1; "
-    GNUPLOT_GRID="set grid; "
-    GNUPLOT_SET=${GNUPLOT_XLABEL}${GNUPLOT_TERMINAL}${GNUPLOT_KEY}${GNUPLOT_ERRORBARS}${GNUPLOT_OFFSET}${GNUPLOT_XTICS}${GNUPLOT_GRID}
     GNUPLOT_LINESTYLE="yerrorlines linewidth 2"
-
-    GNUPLOT_FILE_WTIME=${GNUPLOT_BASENAME}-wtime.gnuplot
+    local GNUPLOT_XLABEL="set xlabel 'nodes'; "
+    local GNUPLOT_TERMINAL="set terminal svg size 640,480 name '$GNUPLOT_BASENAME'; "
+    if [ "$GPU" == true ]; then
+	local GNUPLOT_KEY="set key left top Left; "
+    else
+	local GNUPLOT_KEY="set key left top maxrows 4; "
+    fi
+    local GNUPLOT_ERRORBARS="set errorbars 2.0; "
+    local GNUPLOT_OFFSET="set auto fix; set offsets graph 0.05, 0.05, 0, 0; "
+    local GNUPLOT_XTICS="set xtics 1; "
+    local GNUPLOT_GRID="set grid; "
+    local GNUPLOT_SET=${GNUPLOT_XLABEL}${GNUPLOT_TERMINAL}${GNUPLOT_KEY}${GNUPLOT_ERRORBARS}${GNUPLOT_OFFSET}${GNUPLOT_XTICS}${GNUPLOT_GRID}
+        
+    GNUPLOT_BASENAME=${RUN_DESCRIPTOR//${GPU_OFFLOAD_STR}}
+    #GNUPLOT_FILE_WTIME=${GNUPLOT_BASENAME}-wtime.gnuplot
     GNUPLOT_FILE_NSPERDAY=${GNUPLOT_BASENAME}-nsperday.gnuplot
-    GNUPLOT_FILE_HOURSPERNS=${GNUPLOT_BASENAME}-hoursperns.gnuplot
-    GNUPLOT_FILE_DDLI=${GNUPLOT_BASENAME}-ddli.gnuplot
-    GNUPLOT_FILE_PMELI=${GNUPLOT_BASENAME}-pmeli.gnuplot
+    #GNUPLOT_FILE_HOURSPERNS=${GNUPLOT_BASENAME}-hoursperns.gnuplot
+    #GNUPLOT_FILE_DDLI=${GNUPLOT_BASENAME}-ddli.gnuplot
+    #GNUPLOT_FILE_PMELI=${GNUPLOT_BASENAME}-pmeli.gnuplot
     
-    #echo -e $GNUPLOT_SET"\nset ylabel 'Walltime (seconds)'; set output \"${GNUPLOT_BASENAME}-wtime.svg\"\nplot \\" > $GNUPLOT_FILE_WTIME
-    echo -e $GNUPLOT_SET"\nset yrange [*:$nsperday_max]; set ylabel 'Performance (ns/day)'; set output \"${GNUPLOT_BASENAME}-nsperday.svg\"\nplot \\" > $GNUPLOT_FILE_NSPERDAY
-    #echo -e $GNUPLOT_SET"\nset ylabel 'Performance (hours/ns)'; set output \"${GNUPLOT_BASENAME}-hoursperns.svg\"\nplot \\"  > $GNUPLOT_FILE_HOURSPERNS
-    #echo -e $GNUPLOT_SET"\nset ylabel 'DD load imbalance (% runtime spent waiting)'; set output \"${GNUPLOT_BASENAME}-ddli.svg\"\nplot \\"  > $GNUPLOT_FILE_DDLI
-    #echo -e $GNUPLOT_SET"\nset ylabel 'PME load imbalance (% runtime spent waiting); set output \"${GNUPLOT_BASENAME}-pmeli.svg\"\nplot \\" > $GNUPLOT_FILE_PMELI
+    if [ ! -f "${GNUPLOT_FILE_NSPERDAY}" ]; then
+	#echo -e $GNUPLOT_SET"\nset ylabel 'Walltime (seconds)'; set output \"${GNUPLOT_BASENAME}-wtime.svg\"\nplot \\" > $GNUPLOT_FILE_WTIME
+	echo -e $GNUPLOT_SET"\nset yrange [0:$nsperday_max]; set ylabel 'Performance (ns/day)'; set output \"${GNUPLOT_BASENAME}-nsperday.svg\"\nplot \\" > $GNUPLOT_FILE_NSPERDAY
+	#echo -e $GNUPLOT_SET"\nset ylabel 'Performance (hours/ns)'; set output \"${GNUPLOT_BASENAME}-hoursperns.svg\"\nplot \\"  > $GNUPLOT_FILE_HOURSPERNS
+	#echo -e $GNUPLOT_SET"\nset ylabel 'DD load imbalance (% runtime spent waiting)'; set output \"${GNUPLOT_BASENAME}-ddli.svg\"\nplot \\"  > $GNUPLOT_FILE_DDLI
+	#echo -e $GNUPLOT_SET"\nset ylabel 'PME load imbalance (% runtime spent waiting); set output \"${GNUPLOT_BASENAME}-pmeli.svg\"\nplot \\" > $GNUPLOT_FILE_PMELI
+    fi
 }
 
 
@@ -716,42 +728,45 @@ function gnuplot_init()
 #
 function plot()
 {
-    # Find largest nsperday across all values to set same y-axis scale on each plot
-    nsperday_max=`cut -s -f 4 ${BENCHMARK_NAME}*-nsperday.dat | sort -nr | head -n 1`
+    # Find largest nsperday to set same y-axis scale on each plot
+    #nsperday_max=$(cut -s -f 4 ${BENCHMARK_NAME}${SMT_STR}${RANKSPERNODE_STR}${THREADSPERRANK_STR}*${MDRUN_STR}${EXTRA_INFO}-nsperday.dat | sort -nr | head -n 1)
+    nsperday_max=$(cut -s -f 4 ${BENCHMARK_NAME}*${MDRUN_STR}${EXTRA_INFO}-nsperday.dat | sort -nr | head -n 1)
     
-    GNUPLOT_BASENAME=${BENCHMARK_NAME}${SMT_STR}${MDRUN_STR}${EXTRA_INFO}
-    gnuplot_init
-    
+    #GNUPLOT_BASENAME=${BENCHMARK_NAME}${SMT_STR}${RANKSPERNODE_STR}${THREADSPERRANK_STR}${MDRUN_STR}${EXTRA_INFO}
+        
     echo -n "Plotting "
     
-    for descriptor in `ls -r ${BENCHMARK_NAME}${SMT_STR}*${MDRUN_STR}${EXTRA_INFO}-nsperday.dat`
+    for descriptor in ${BENCHMARK_NAME}${SMT_STR}${RANKSPERNODE_STR}${THREADSPERRANK_STR}*${MDRUN_STR}${EXTRA_INFO}-nsperday.dat
     do
 	echo -n "."
-	descriptor=`basename $descriptor "-nsperday.dat"`
+	descriptor=$(basename $descriptor "-nsperday.dat")
 	descriptor_to_options $descriptor
 	init_params
+	gnuplot_init
 	
-	offload_legend=""
+	local offload_legend_title=""
+	local legend_title=""
 	if [ "$GPU" == true ]; then
-	    offload_legend="CPU only"
-	    if [ "$NB" == "gpu" ]; then offload_legend=", nonbonded"; fi
-	    if [ "$PME" == "gpu" ]; then offload_legend=${offload_legend}", PME"; fi
-    	    if [ "$BONDED" == "gpu" ]; then offload_legend=${offload_legend}", bonded"; fi
-	    if [ "$UPDATE" == "gpu" ]; then offload_legend=${offload_legend}", update"; fi
+	    offload_legend_title="CPU only"
+	    if [ "${NB}" == "gpu" ]; then offload_legend_title="nonbonded"; fi
+	    if [ "${PME}" == "gpu" ]; then offload_legend_title=${offload_legend_title}", PME"; fi
+    	    if [ "${BONDED}" == "gpu" ]; then offload_legend_title=${offload_legend_title}", bonded"; fi
+	    if [ "${UPDATE}" == "gpu" ]; then offload_legend_title=${offload_legend_title}", update"; fi
+	    legend_title=${offload_legend_title}
+	else
+	    legend_title="${RANKSPERNODE} mpi x ${THREADSPERRANK} omp"
 	fi
-	
-	
-	#echo "\"${descriptor}-wtime.dat\" with ${GNUPLOT_LINESTYLE} title '$RANKSPERNODE mpi x $THREADSPERRANK omp${offload_legend}',\\" >> $GNUPLOT_FILE_WTIME
-	echo "\"${descriptor}-nsperday.dat\" with ${GNUPLOT_LINESTYLE} title '$RANKSPERNODE mpi x $THREADSPERRANK omp${offload_legend}',\\" >> $GNUPLOT_FILE_NSPERDAY
-	#echo "\"${descriptor}-hoursperns.dat\" with ${GNUPLOT_LINESTYLE} title '$RANKSPERNODE mpi x $THREADSPERRANK omp${offload_legend}',\\" >> $GNUPLOT_FILE_HOURSPERNS
-    	#echo "\"${descriptor}-ddli.dat\" with ${GNUPLOT_LINESTYLE} title '$RANKSPERNODE mpi x $THREADSPERRANK omp${offload_legend}',\\" >> $GNUPLOT_FILE_DDLI
-    	#echo "\"${descriptor}-pmeli.dat\" with ${GNUPLOT_LINESTYLE} title '$RANKSPERNODE mpi x $THREADSPERRANK omp${offload_legend}',\\" >> $GNUPLOT_FILE_PMELI
+
+	#echo "\"${descriptor}-wtime.dat\" with ${GNUPLOT_LINESTYLE} title '${legend_title}',\\" >> ${GNUPLOT_FILE_WTIME}
+	echo "\"${descriptor}-nsperday.dat\" with ${GNUPLOT_LINESTYLE} title '${legend_title}',\\" >> ${GNUPLOT_FILE_NSPERDAY}
+	#echo "\"${descriptor}-hoursperns.dat\" with ${GNUPLOT_LINESTYLE} title '${legend_title}',\\" >> ${GNUPLOT_FILE_HOURSPERNS}
+    	#echo "\"${descriptor}-ddli.dat\" with ${GNUPLOT_LINESTYLE} title '${legend_title}',\\" >> ${GNUPLOT_FILE_DDLI}
+    	#echo "\"${descriptor}-pmeli.dat\" with ${GNUPLOT_LINESTYLE} title '${legend_title}',\\" >> ${GNUPLOT_FILE_PMELI}
     done
     
-    truncate -s-3 ${GNUPLOT_BASENAME}*.gnuplot
-    for file in ${GNUPLOT_BASENAME}*.gnuplot; do echo "" >> $file; done
-    
-    gnuplot ${GNUPLOT_BASENAME}*.gnuplot
+    truncate -s-3 ${BENCHMARK_NAME}${SMT_STR}*.gnuplot
+    for file in ${BENCHMARK_NAME}${SMT_STR}*.gnuplot; do echo "" >> $file; done
+    gnuplot ${BENCHMARK_NAME}${SMT_STR}*.gnuplot
     echo "done"
 }
 
